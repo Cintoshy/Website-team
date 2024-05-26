7fca0cee470279106803f9e27f7399accb12a974
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2024-05-26 12:45:56 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import "./App.css";
 import AppBar from "./components/AppBar";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <>
       <AppBar />
+      <Home />
+      <Home />
+      <Home />
+      <Home />
+      <Home />
     </>
   );
 };
---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="h-screen flex items-center">
+      <div className="w-full flex flex-col items-center px-4 md:px-96">
+        <div className="text-lg">Development Website With Us</div>
+        <div className="text-[30px] md:text-[50px] text-center font-bold text-slate-900 lg:leading-[55px] my-5">
+          Web Development and Maintenance Website
+        </div>
+        <div className="text-center">
+          Lorem ipsum dolor sit amet consectetur. Gravida felis habitasse sed
+          platea molestie donec vulputate lectus.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
